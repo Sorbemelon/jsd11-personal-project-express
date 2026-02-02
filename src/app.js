a5da6d4abf7a7e3ebd1826a5be565c35acec87d2
sorbemelon <67460064+Sorbemelon@users.noreply.github.com>
2026-02-02 22:10:42 +0700

    add allowed origin (vercel) to cors

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
+      "https://jsd11-methus-project.vercel.app"
     ],
     credentials: true,
   })
